41ee08eadfae15864b21cba47791a735fc826913
s1mba93 <danielelson93@gmail.com>
2023-05-11 19:21:38 +0100

    Create Collection

--- MessageGenerator.js
+++ MessageGenerator.js
@@ -2,3 +2,9 @@ function randNumGen(num) {
     //Generate a random number
     return Math.floor(Math.random() * num);
 }
+
+const collection = {
+    sign: ['star', 'moon', 'planet', 'comet'],
+    fortune: ['rubbish luck', 'bad luck', 'ok luck', 'good luck', 'excellent luck'],
+    advice: ['play a game', 'eat some food', 'trust no one', 'get some rest', 'touch grass']
+}
