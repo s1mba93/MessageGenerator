635a9b491958554c2b886b40e73cccc0b0ad0db4
s1mba93 <danielelson93@gmail.com>
2023-05-11 19:34:34 +0100

    Created main function for generating the message

--- MessageGenerator.js
+++ MessageGenerator.js
@@ -8,3 +8,29 @@ const collection = {
     fortune: ['rubbish luck', 'bad luck', 'ok luck', 'good luck', 'excellent luck'],
     advice: ['play a game', 'eat some food', 'trust no one', 'get some rest', 'touch grass']
 }
+
+//Array for storing message answers
+let message = [];
+
+//Main function for generating message
+for (let prop in collection) {
+    let optionId = randNumGen(collection[prop].length);
+
+    switch (prop) {
+        case 'sign':
+            message.push(`Your sign right now is a ${collection[prop][optionId]}`);
+            break
+        case 'fortune':
+            message.push(`You have ${collection[prop][optionId]} right now`);
+            break
+        case 'advice':
+            message.push(`you should ${collection[prop][optionId]}`);
+            break
+        default:
+            message.push('There was an error. Please try again');
+    }
+}
+
+
+
+
